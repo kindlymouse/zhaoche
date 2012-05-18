b573d35aeecdbfd13939518d37b0d1e547b48c96
kindlymouse <kindlymouse@gmail.com>
2012-05-18 16:30:13 +0800

    commit first

---
+++ application/controllers/Register.php
@@ -0,0 +1,16 @@
+<?php
+
+class Register extends RC_Controller {
+
+	function  __construct()  {
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		
+	}
+	
+}
+
+?>
---
+++ application/core/RC_Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class RC_Controller extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+	}
+	
+}
+
+?>
